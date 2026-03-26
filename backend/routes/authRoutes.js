973cb044cd72fafbce959c290b1f5808d748b331
Nallamangala sravanth kumar <sravanthkumar910@gmail.com>
2026-03-26 19:33:15 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser, verifyOTP } = require("../controllers/authController.js");
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/verify-otp", verifyOTP);
-
-module.exports = router;
